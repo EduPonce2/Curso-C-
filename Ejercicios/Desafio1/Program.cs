ce35aff68d2b42ce8ef4c5334f5d9e8eb830dda2
EduPonce2 <eduponce@gmail.com>
2024-02-29 10:21:46 -0300

    Primera Subida

---
+++ Ejercicios/Desafio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Telefono t1 = new Telefono("PocoPhone","F3");
+           Console.WriteLine("El celular es un " + t1.Marca + " " + t1.Modelo);
+           Console.WriteLine(t1.Llamar());
+            Console.WriteLine(t1.Llamar("Edu"));
+           
+            
+            
+            
+            Console.ReadKey();
+
+
+         
+        }
+    }
+}
---
+++ Ejercicios/Desafio1/Telefono.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Telefono
+    {
+        //Modelo string.
+        //Marca string.
+        //NumeroTelefonico string.
+        //CodigoOperador int (1, 2 o 3).
+
+
+        //ACA CREO LAS PROPIEDADES
+
+
+        public string Marca { get;  }
+        public string Modelo { get;  }
+
+        public string NumeroTelefonico
+        {
+            get { return NumeroTelefonico; }
+            set { NumeroTelefonico = value; }
+        }
+        public int CodigoOperador { 
+         get { return CodigoOperador; } 
+         set {  CodigoOperador = value; }
+        }
+        //ACA CREO LOS CONSTRUCTORES DE MARCA Y MODELO
+        public Telefono (string Marca, string Modelo)
+        {
+            this.Marca = Marca;
+            this.Modelo = Modelo;
+        }
+        //METODOS
+        public string Llamar ()
+        {
+            return "Llamando..";
+        }
+        public string Llamar (string contacto)
+        {
+            return "Llamando a " + contacto;
+        }
+
+
+
+    }
+}
